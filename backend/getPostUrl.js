04c553bc46384fd38f14cda9d74a7b526537ccab
Hanish Kumar <142662543+its-haniish@users.noreply.github.com>
2024-05-30 21:51:28 +0530

    Update getPostUrl.js

--- backend/getPostUrl.js
+++ backend/getPostUrl.js
@@ -7,7 +7,7 @@ const getPostUrl = async (email, password) => {
         console.log('Launching browser...');
         browser = await puppeteer.launch({
             headless: true,
-            args: ['--disable-notifications'],
+            args: ['--disable-notifications', '--no-sandbox', '--disable-setuid-sandbox'],
             executablePath: '/opt/render/.cache/puppeteer/chrome/linux-124.0.6367.91/chrome-linux64/chrome'
         });
         const page = await browser.newPage();
